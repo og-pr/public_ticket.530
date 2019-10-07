f41f0d0192b3bd7baf886b45df3da6b4a676d235
Otto Grajeda <ottograjeda@gmail.com>
2019-10-07 16:26:38 -0700

    Update

---
+++ SimpleLanding/index.ios.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+
+console.disableYellowBox = true; // disable warning regarding componentWillMount is deprecated and will be removed in the next major version
+
+import App from './app/components/App/'; // for monorepo
+AppRegistry.registerComponent('SimpleLanding', () => App);
